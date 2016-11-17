48e9661213d2c96f3acdc7afa4d8007af62e8d77
jlasherass <jaimelasheras94@gmail.com>
2016-11-17 16:37:13 +0100

    Creamos un proyecto con un ejemplo de formulario de alta completo

---
+++ index.php
@@ -0,0 +1,12 @@
+//ejemplo formulario un poco completo
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejemplo de formulario de alta</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
