c7088f96133656d0a5b67a8eb2be587594ec2dbf
jlasherass <jaimelasheras94@gmail.com>
2016-11-17 16:47:33 +0100

    Formulario html creado

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
-//ejemplo formulario un poco completo
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Ejemplo de formulario de alta</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="datos.php" method="POST">
+            <p>Nombre: <input type="text" name="nombre"></p>
+            <p>Edad: <input type="number" name="edad"></p>
+            <p>Ciudad: <select name="ciudad"></p>
+                <option> Barcelona </option>
+                <option> Girona </option>
+                <option> Lleida </option>
+                <option> Tarragona </option>
+            </select>
+            <p>Sexo:
+            Hombre <input type="radio" name="sexo" value="hombre">
+            Mujer <input type="radio" name="sexo" value="hombre">
+            </p>
+            <input type="submit" name="enviar" value="Enviar datos">           
     </body>
 </html>
